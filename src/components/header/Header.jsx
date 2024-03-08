28fc8f9dd6e94d12ed574fd9c7725eec11c4ff3b
ThapananKulab <nicekrubma123@gmail.com>
2024-03-09 00:44:34 +0700

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,47 +1,44 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 import './header.css'
 import CTA from './CTA'
 import port from '../../assets/port3.png'
 import HeaderSocials from './HeaderSocials'
 
 const Home = () => {
-  const [typedText, setTypedText] = useState('');
-  const [hasTyped, setHasTyped] = useState(false);
-  const originalText = "I'm Thapanan Kulab";
+  const [typedText, setTypedText] = useState('')
+  const [hasTyped, setHasTyped] = useState(false)
+  const originalText = "I'm Thapanan Kulab"
 
   useEffect(() => {
     if (!hasTyped) {
-      let index = 0;
+      let index = 0
 
       function type() {
-        setTypedText(originalText.substring(0, index));
-        index++;
+        setTypedText(originalText.substring(0, index))
+        index++
 
         if (index <= originalText.length) {
-          setTimeout(type, 100);  // Adjust the timeout for typing speed
+          setTimeout(type, 100) // Adjust the timeout for typing speed
         } else {
-          setHasTyped(true);
+          setHasTyped(true)
         }
       }
 
-      type();
+      type()
     }
-  }, [hasTyped, originalText]);
+  }, [hasTyped, originalText])
 
   return (
-    <header id='home'>
+    <header id="home">
       <div className="container header__container">
         <h5>Hello</h5>
         <h1>{typedText}</h1>
         <h2>Software Engineer</h2>
         <CTA />
         <HeaderSocials />
-        <div className="me">
-          <img src={port} alt='me' />
-        </div>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
