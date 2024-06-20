993a98ab44902334e150c7be112d61f32d4f8296
thapanankulab25 <nicekrubma123@gmail.com>
2024-06-20 14:35:06 +0700

    update

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,9 +1,9 @@
 import React, { useRef, useEffect } from "react"
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
 import "leaflet/dist/leaflet.css"
-import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css" // Re-uses images from ~leaflet package
-import * as L from "leaflet"
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css"
 import "leaflet-defaulticon-compatibility"
+import Navbar from "../components/header"
 
 const MapComponent = () => {
   const mapRef = useRef()
@@ -16,24 +16,27 @@ const MapComponent = () => {
   }, [])
 
   return (
-    <div style={{ width: "100%", height: "500px" }}>
-      <MapContainer
-        center={[13.8113048, 100.5049658]} // ตำแหน่งศูนย์กลางแผนที่
-        zoom={13}
-        scrollWheelZoom={false}
-        style={{ height: "100%", width: "100%" }}
-        ref={mapRef}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[13.8113048, 100.5049658]}>
-          <Popup>
-            ตำแหน่งที่ต้องการปักหมุด <br /> [13.811387, 100.505121]
-          </Popup>
-        </Marker>
-      </MapContainer>
+    <div>
+      <Navbar />
+      <div style={{ width: "100%", height: "500px" }}>
+        <MapContainer
+          center={[13.811202, 100.504995]} // ตำแหน่งศูนย์กลางแผนที่
+          zoom={13}
+          scrollWheelZoom={false}
+          style={{ height: "100%", width: "100%" }}
+          ref={mapRef}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[13.811202, 100.504995]}>
+            <Popup>
+              ตำแหน่งที่ต้องการปักหมุด <br /> [13.811387, 100.505121]
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
     </div>
   )
 }
