2b7592f2230bb026dee28d930fe8596a99a447d6
ThapananKulab <nicekrubma123@gmail.com>
2024-07-09 22:04:26 +0700

    Update modal-resume.js

--- src/components/modal-resume.js
+++ src/components/modal-resume.js
@@ -1,4 +1,4 @@
-// Modal.js
+// components/modal-resume.js
 
 import React from "react"
 
@@ -6,19 +6,41 @@ const Modal = ({ isOpen, imageUrl, onClose }) => {
   const handleCloseModal = () => {
     onClose()
   }
+
   if (!isOpen) return null
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
-      <div className="relative w-auto max-w-3xl mx-auto my-6">
-        <div className="modal-content">
-          <img src={imageUrl} alt="Modal" className="w-full h-auto" />
+    <div
+      className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center z-50 bg-black bg-opacity-50"
+      onClick={handleCloseModal}
+    >
+      <div
+        className="modal-container bg-white p-8 rounded-lg shadow-lg max-w-3xl w-full"
+        onClick={e => e.stopPropagation()}
+      >
+        <div className="flex justify-end">
           <button
-            className="absolute top-0 right-0 m-4 text-3xl text-gray-500"
             onClick={onClose}
+            className="text-gray-500 hover:text-gray-700 focus:outline-none"
           >
-            &times;
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
           </button>
+        </div>
+        <div className="modal-content">
+          <img src={imageUrl} alt="Resume" className="w-full" />
         </div>
       </div>
     </div>
