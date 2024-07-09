6380bd5ce91fd3b459eaf241e7ec47d2fe64f1dd
thapanankulab25 <nicekrubma123@gmail.com>
2024-07-09 15:45:11 +0700

    Update project.js

--- src/pages/project.js
+++ src/pages/project.js
@@ -6,24 +6,29 @@ import { GrView } from "react-icons/gr"
 
 const projectData = [
   {
-    degree: "Coffee Shop Raw Material Management System",
+    title: "Coffee Shop Raw Material Management System",
     tools: "MongoDB, React, Express.js, NodeJS,",
     details: "Graduated with honors, specialized in software development.",
     github: "",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWNs7_TqH_iYq6zlZnQcT3XRUjK1cLRT58RQ&s",
     link: "https://pos-management-cafe.netlify.app/sale/pos",
   },
   {
-    degree: "Master of Science in Data Science",
+    title: "Time Attendance Delta Company",
     tools: "MongoDB, React, NodeJS,",
-    details: "Research focused on machine learning and big data.",
+    details:
+      "Time entry and exit records: Time records record the time employees come to work and leave the workplace.",
+    github: "https://github.com/ThapananKulab/work-io-demo.git",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO4gVxYgRiRzBCfNkW-JWcz6YxkTrlNao6iQ&s",
+    link: "https://ambitious-desert-0548dee10.5.azurestaticapps.net/",
   },
   {
-    degree: "Master of Science in Data Science",
+    title: "Master of Science in Data Science",
     tools: "2020-2022",
     details: "Research focused on machine learning and big data.",
   },
   {
-    degree: "Master of Science in Data Science",
+    title: "Master of Science in Data Science",
     tools: "2020-2022",
     details: "Research focused on machine learning and big data.",
   },
@@ -41,9 +46,10 @@ const ProjectPage = () => {
             <div className="grid gap-6 mb-10 md:grid-cols-2">
               {projectData.map((project, index) => (
                 <div key={index} className="bg-white p-6 rounded-lg shadow-lg">
-                  <h2 className="text-2xl font-bold mb-2">{project.degree}</h2>
+                  <h2 className="text-2xl font-bold mb-2">{project.title}</h2>
                   <p className="text-gray-500">Tools: {project.tools}</p>
                   <p className="text-gray-600 mt-4">{project.details}</p>
+
                   <div className="card-actions justify-center">
                     <a href={project.link} target="_blank">
                       <button className="btn btn-outline btn-dark">
